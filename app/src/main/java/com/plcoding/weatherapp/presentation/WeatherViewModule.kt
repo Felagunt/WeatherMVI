7b218bc3a56dee0ded1bfcb38757d05ac92b99b4
Ilya <felagunt@live.ru>
2022-12-14 15:19:30 +0300

    resource respond

--- app/src/main/java/com/plcoding/weatherapp/presentation/WeatherViewModule.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/WeatherViewModule.kt
@@ -12,6 +12,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+
 @HiltViewModel
 class WeatherViewModule @Inject constructor(
     private val repository: WeatherRepository,
